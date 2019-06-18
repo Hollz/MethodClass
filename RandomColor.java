0a95cbce785280c37e0aa3bbdc31fb80c213b88e
joy <466427193@qq.com>
2019-06-18 15:36:58 +0800

    add randomColor

---
+++ RandomColor.java
@@ -0,0 +1,10 @@
+class RandomColor{
+
+ public static int randomColor() {
+        int red = (int)(Math.random() * 128);
+        int green = (int)(Math.random() * 128);
+        int blue = (int)(Math.random() * 128);
+        return 0xFF000000 | (red << 16) | (green << 8) | blue;
+    }
+}
+ 
